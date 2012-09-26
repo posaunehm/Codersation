4ecf799ae5d943c790706cc6e8c5f314422f9f17
Shinsuke Abe <mao.instantlife@gmail.com>
2012-09-26 23:32:18 +0900

    add stock test.

---
+++ TDDBC_VendorMachine/ShinsukeAbe/products/VendingMachine/src/java/Juice.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mao
+ */
+public class Juice {
+    private String name;
+    private Integer price;
+    
+    public Juice(String name, Integer price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+    
+}
