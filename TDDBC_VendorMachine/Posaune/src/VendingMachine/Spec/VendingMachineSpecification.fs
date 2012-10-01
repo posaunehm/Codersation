9f6b2f428aca1487e077177f0b12aa90d11b601a
posaunehm <posaunehm@gmail.com>
2012-10-02 08:52:22 +0900

    お金の複数回投入に対応

--- TDDBC_VendorMachine/Posaune/src/VendingMachine/Spec/VendingMachineSpecification.fs
+++ TDDBC_VendorMachine/Posaune/src/VendingMachine/Spec/VendingMachineSpecification.fs
@@ -13,21 +13,26 @@ let total_amount amount (vm:VendingMachine)  =
 
 let insert_money amount (vm:VendingMachine) = 
     printMethod amount
-    vm.InsertMoeny(amount)
+    amount |> List.iter vm.InsertMoeny
     vm
  
 //Feature:‚¨‹à‚ª“Š“ü‚Å‚«‚é
-let sut1 = new VendingMachine();
 [<Scenario>]
 let ``After inserting 10 yen, it's total amount is 10``() =
   Given (new VendingMachine())          
-    |> When insert_money 10      
+    |> When insert_money [10]      
     |> It should have (total_amount 10)
+    |> Verify
+
+[<Scenario>]
+let ``After inserting 10 yen and 100 yen, it's total amount is 110``() =
+  Given (new VendingMachine())          
+    |> When insert_money [10;100]  
+    |> It should have (total_amount 110)
     |> Verify
                  
    
 //Feature:“Š“ü‹àŠz‚ÌŠm”F‚ª‚Å‚«‚é
-let sut2 = new VendingMachine();
 [<Scenario>]
 let ``Initially, the total amount of this vending machine is 0``() =
   Given (new VendingMachine())                
