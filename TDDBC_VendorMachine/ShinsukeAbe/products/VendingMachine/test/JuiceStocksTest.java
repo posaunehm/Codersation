4d3c04457ba58c049a8771d317f2694191af1e89
Shinsuke Abe <mao.instantlife@gmail.com>
2012-10-02 20:55:22 +0900

    add get method test null value

--- TDDBC_VendorMachine/ShinsukeAbe/products/VendingMachine/test/JuiceStocksTest.java
+++ TDDBC_VendorMachine/ShinsukeAbe/products/VendingMachine/test/JuiceStocksTest.java
@@ -10,6 +10,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.*;
+import static org.hamcrest.core.IsNull.*;
 
 /**
  *
@@ -18,6 +19,7 @@ import static org.hamcrest.core.Is.*;
 public class JuiceStocksTest {
     
     private JuiceStocks juiceStocks;
+    private JuiceStock defaultStock;
     
     private Juice cola = new Juice("コーラ", 120);
     private Juice redBull = new Juice("レッドブル", 200);
@@ -36,20 +38,18 @@ public class JuiceStocksTest {
     @Before
     public void setUp() {
         juiceStocks = JuiceStocksFactory.createNewStocks();
+        defaultStock = juiceStocks.getAll().get(0);
     }
     
     @After
     public void tearDown() {
     }
     
-    // TODO 
-    // TODO 初期状態でレッドブルを指定するとストックを取得できない
     // TODO 投入金額不足 <= もっと上位のサービスかアプリケーション層
     
     @Test
     public void ストックの初期状態ではコーラが5本格納されている() {
-        JuiceStock juiceStock = juiceStocks.getAll().get(0);
-        assertThat(juiceStock, is(new JuiceStock(cola, 5)));
+        assertThat(defaultStock, is(new JuiceStock(cola, 5)));
     }
     
     @Test
@@ -61,36 +61,31 @@ public class JuiceStocksTest {
     
     @Test
     public void 投入金額がnullの場合はコーラが購入不可能() {
-        JuiceStock juiceStock = juiceStocks.getAll().get(0);
-        assertThat(juiceStock.isPurchasable(null), is(false));
+        assertThat(defaultStock.isPurchasable(null), is(false));
     }
     
     @Test
     public void 初期状態で100円投入した場合はコーラが購入不可能() {
-        JuiceStock juiceStock = juiceStocks.getAll().get(0);
-        assertThat(juiceStock.isPurchasable(100), is(false));
+        assertThat(defaultStock.isPurchasable(100), is(false));
     }
     
     @Test
     public void 初期状態で120円投入した場合はコーラが購入可能() {
-        JuiceStock juiceStock = juiceStocks.getAll().get(0);
-        assertThat(juiceStock.isPurchasable(120), is(true));
+        assertThat(defaultStock.isPurchasable(120), is(true));
     }
     
     @Test
     public void 初期状態からコーラの出庫を行うと在庫が4つになる() {
-        JuiceStock juiceStock = juiceStocks.getAll().get(0);
-        juiceStock.delivery();
-        assertThat(juiceStock.getCount(), is(4));
+        defaultStock.delivery();
+        assertThat(defaultStock.getCount(), is(4));
     }
     
     @Test
     public void コーラの在庫が0の状態で120円投入した場合は購入不可能() {
-        JuiceStock juiceStock = juiceStocks.getAll().get(0);
         for(int i = 0; i < 5; i++) {
-            juiceStock.delivery();
+            defaultStock.delivery();
         }
-        assertThat(juiceStock.isPurchasable(120), is(false));
+        assertThat(defaultStock.isPurchasable(120), is(false));
     }
     
     @Test
@@ -98,5 +93,11 @@ public class JuiceStocksTest {
         JuiceStock juiceStock = juiceStocks.get(cola);
         assertThat(juiceStock.getJuice(), is(cola));
         assertThat(juiceStock.getCount(), is(5));
+    }
+    
+    @Test
+    public void 初期状態でレッドブルを指定するとストックを取得できない() {
+        JuiceStock juiceStock = juiceStocks.get(redBull);
+        assertThat(juiceStock, nullValue());
     }
 }
