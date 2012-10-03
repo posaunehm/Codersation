b661a4fa18dc6a79433799b9d2a68c770007cf39
Ritalin <kazu.ktz.tamura@gmail.com>
2012-10-03 20:07:37 +0900

    なんか変えてた

---
+++ TDDBC_VendorMachine/ktz_alias/sources/VendingMachine.Test/PurchaseTest.cs
@@ -0,0 +1,13 @@
+using System;
+
+using NUnit.Framework;
+
+namespace VendingMachine.Test {
+    [TestFixture]
+    public class PurchaseTest {
+        [Test]
+        public void () {
+        }
+    }
+}
+
---
+++ TDDBC_VendorMachine/ktz_alias/sources/VendingMachine/Model/PurchaseContext.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace VendingMachine {
+    public class PurchaseContext {
+        public PurchaseContext() {
+        }
+    }
+}
+
