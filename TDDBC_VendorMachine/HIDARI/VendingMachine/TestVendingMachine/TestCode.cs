1181d6cbfcd5113e75c731e882b2a9a58f7a58b9
sho ikawa <ika5140@gmail.com>
2012-10-07 17:19:57 +0900

    課題のステップ0の一つ目のコードを追加

--- TDDBC_VendorMachine/HIDARI/VendingMachine/TestVendingMachine/TestCode.cs
+++ TDDBC_VendorMachine/HIDARI/VendingMachine/TestVendingMachine/TestCode.cs
@@ -10,10 +10,27 @@ namespace TestVendingMachine
     public class TestCode
     {
 		[Test]
-		public void JuiceStocksCanAddNewJuice(){
-			Juice juice = new Juice();
-			Assert.That(juice,Has.Property("Name"));
+		public void InsertCoinsToCoinMech(){
+			CoinMech coinMech = new CoinMech();
 
+			coinMech.Ten = new Yen10();
+			coinMech.Fifty = new Yen50();
+			coinMech.Hundred = new Yen100();
+			coinMech.FiveH = new Yen500();
+
+			Assert.That(coinMech.Ten.Value,Is.EqualTo(10));
+			Assert.That(coinMech.Fifty.Value,Is.EqualTo(50));
+			Assert.That(coinMech.Hundred.Value,Is.EqualTo(100));
+			Assert.That(coinMech.FiveH.Value,Is.EqualTo(500));
+		}
+
+		[Test]
+		public void InsertBillToBillVali(){
+			BillVali billVali = new BillVali();
+
+			billVali.Thousand = new Yen1000();
+
+			Assert.That(billVali.Thousand.Value,Is.EqualTo(1000));
 		}
     }
 }
