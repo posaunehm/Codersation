ddd2946dbd8a5d3fc2a1b64489f44c3b28205952
irof <irof@javaring.info>
2012-10-09 12:47:55 +0900

    CreditService.getUseMoneyListをMoneyStockに移動

--- TDDBC_VendorMachine/irof/src/main/java/net/codersation/vendingmachine/CreditService.java
+++ TDDBC_VendorMachine/irof/src/main/java/net/codersation/vendingmachine/CreditService.java
@@ -1,35 +1,15 @@
 package net.codersation.vendingmachine;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+
+import net.codersation.vendingmachine.moneyflow.MoneyStock;
 
 public class CreditService {
 
+	/**
+	 * @deprecated Use {@link MoneyStock#getUseMoneyList(List<Money>,int)} instead
+	 */
 	public static List<Money> getUseMoneyList(List<Money> list, int i) {
-
-		List<Money> result = new ArrayList<>();
-
-		Collections.sort(list);
-		for (Money money : list) {
-			if (i <= 0) {
-				break;
-			}
-			if (money.getValue() < i) {
-				result.add(money);
-				i -= money.getValue();
-			} else {
-				result.add(money);
-				i -= money.getValue();
-			}
-		}
-
-		for (Money money : result.toArray(new Money[0])) {
-			if (i <= money.getValue() * -1) {
-				result.remove(money);
-				i += money.getValue();
-			}
-		}
-		return result;
+		return MoneyStock.getUseMoneyList(list, i);
 	}
 }
--- TDDBC_VendorMachine/irof/src/main/java/net/codersation/vendingmachine/moneyflow/MoneyStock.java
+++ TDDBC_VendorMachine/irof/src/main/java/net/codersation/vendingmachine/moneyflow/MoneyStock.java
@@ -1,6 +1,8 @@
 package net.codersation.vendingmachine.moneyflow;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import net.codersation.vendingmachine.Money;
 
@@ -12,5 +14,42 @@ public class MoneyStock extends ArrayList<Money> {
 			amount += c.getValue();
 		}
 		return amount;
+	}
+
+	public List<Money> getUseMoneyList(int i) {
+
+		List<Money> result = new ArrayList<>();
+
+		Collections.sort(this);
+		for (Money money : this) {
+			if (i <= 0) {
+				break;
+			}
+			if (money.getValue() < i) {
+				result.add(money);
+				i -= money.getValue();
+			} else {
+				result.add(money);
+				i -= money.getValue();
+			}
+		}
+
+		for (Money money : result.toArray(new Money[0])) {
+			if (i <= money.getValue() * -1) {
+				result.remove(money);
+				i += money.getValue();
+			}
+		}
+		return result;
+
+	}
+
+	/**
+	 * @deprecated 一時的に残す
+	 */
+	public static List<Money> getUseMoneyList(List<Money> list, int i) {
+		MoneyStock stock = new MoneyStock();
+		stock.addAll(list);
+		return stock.getUseMoneyList(i);
 	}
 }
