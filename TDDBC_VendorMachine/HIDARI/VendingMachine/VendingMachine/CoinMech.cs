0263ced446689d5d66f5bb17715f6fa5cc116c1b
sho ikawa <ika5140@gmail.com>
2012-10-13 16:19:20 +0900

    試行錯誤錯乱中

---
+++ TDDBC_VendorMachine/HIDARI/VendingMachine/VendingMachine/CoinMech.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace VendingMachine
+{
+	public class CoinMech
+	{
+		public CoinMech ()
+		{
+		}
+
+		public void Inserted(Money jpy){
+
+		}
+
+		public int ShowAmount(){
+			return 0;
+		}
+	}
+}
+
---
+++ TDDBC_VendorMachine/HIDARI/VendingMachine/VendingMachine/Money.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VendingMachine
+{
+	public class Money{
+		public int Amount{get; private set;}
+
+		public Money(int amount){
+			this.Amount = amount;
+		}
+	}
+}
