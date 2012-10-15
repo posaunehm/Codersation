5ed4e77cedf977d3e35ae6e9b4c531a9545b75bd
irof <irof@javaring.info>
2012-10-15 21:43:36 +0900

    getUseMoneyListのテストをtakeOutのテストに変更

--- TDDBC_VendorMachine/irof/src/test/java/net/codersation/vendingmachine/moneyflow/MoneyStockTest.java
+++ TDDBC_VendorMachine/irof/src/test/java/net/codersation/vendingmachine/moneyflow/MoneyStockTest.java
@@ -2,11 +2,6 @@ package net.codersation.vendingmachine.moneyflow;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.junit.matchers.JUnitMatchers.everyItem;
-import static org.junit.matchers.JUnitMatchers.hasItems;
-
-import java.util.List;
-
 import net.codersation.vendingmachine.Money;
 
 import org.junit.Test;
@@ -22,27 +17,24 @@ public class MoneyStockTest {
 	}
 
 	@Test
-	public void 百円三枚あるところで120円の買い物をすると百円二枚使う() {
+	public void 百円三枚から120円を取り出すと200円() {
 		add(Money.HundredYen, Money.HundredYen, Money.HundredYen);
-		List<Money> actual = sut.getUseMoneyList(120);
-		assertThat(actual.size(), is(2));
-		assertThat(actual, everyItem(is(Money.HundredYen)));
+		MoneyStock actual = sut.takeOut(120);
+		assertThat(actual.getAmount(), is(200));
 	}
 
 	@Test
-	public void 百円二枚と五十円一枚あるところで120円の買い物をすると百円と五十円を使う() {
+	public void 百円二枚と五十円一枚から120円を取り出すと150円() {
 		add(Money.HundredYen, Money.HundredYen, Money.FiftyYen, Money.TenYen);
-		List<Money> actual = sut.getUseMoneyList(120);
-		assertThat(actual.size(), is(2));
-		assertThat(actual, hasItems(Money.HundredYen, Money.FiftyYen));
+		MoneyStock actual = sut.takeOut(120);
+		assertThat(actual.getAmount(), is(150));
 	}
 
 	@Test
-	public void 百円二枚と十円三枚あるところで120円の買い物をすると百円と十円二枚を使う() {
+	public void 百円二枚と十円三枚から120円を取り出すと120円() {
 		add(Money.HundredYen, Money.HundredYen, Money.TenYen, Money.TenYen, Money.TenYen);
-		List<Money> actual = sut.getUseMoneyList(120);
-		assertThat(actual.size(), is(3));
-		assertThat(actual, hasItems(Money.HundredYen, Money.TenYen, Money.TenYen));
+		MoneyStock actual = sut.takeOut(120);
+		assertThat(actual.getAmount(), is(120));
 	}
 
 }
