4b0b035c046c8ed3a61340b12f6acff88a46ff80
irof <irof@javaring.info>
2012-10-15 20:29:15 +0900

    MoneyStockをArrayList継承クラスじゃなくす

--- TDDBC_VendorMachine/irof/src/main/java/net/codersation/vendingmachine/moneyflow/MoneyStock.java
+++ TDDBC_VendorMachine/irof/src/main/java/net/codersation/vendingmachine/moneyflow/MoneyStock.java
@@ -6,22 +6,44 @@ import java.util.List;
 
 import net.codersation.vendingmachine.Money;
 
-public class MoneyStock extends ArrayList<Money> {
+public class MoneyStock {
+
+	private ArrayList<Money> stock = new ArrayList<>();
 
 	public int getAmount() {
 		int amount = 0;
-		for (Money c : this) {
+		for (Money c : stock) {
 			amount += c.getValue();
 		}
 		return amount;
+	}
+
+	void add(Money e) {
+		stock.add(e);
+	}
+
+	public void addAll(List<Money> l) {
+		stock.addAll(l);
+	}
+
+	void addAll(MoneyStock s) {
+		stock.addAll(s.stock);
+	}
+
+	void remove(Money e) {
+		stock.remove(e);
+	}
+
+	void clear() {
+		stock.clear();
 	}
 
 	public List<Money> getUseMoneyList(int i) {
 
 		List<Money> result = new ArrayList<>();
 
-		Collections.sort(this);
-		for (Money money : this) {
+		Collections.sort(stock);
+		for (Money money : stock) {
 			if (i <= 0) {
 				break;
 			}
