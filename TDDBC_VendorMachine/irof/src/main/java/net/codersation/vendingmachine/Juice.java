87a7f8ed7c0bddc16b051a3210d85bae08dcdbab
irof <irof@javaring.info>
2012-10-29 20:03:30 +0900

    Juiceにequals/hashCode追加

--- TDDBC_VendorMachine/irof/src/main/java/net/codersation/vendingmachine/Juice.java
+++ TDDBC_VendorMachine/irof/src/main/java/net/codersation/vendingmachine/Juice.java
@@ -1,6 +1,5 @@
 package net.codersation.vendingmachine;
 
-
 public class Juice {
 	private final String juiceName;
 	private final int price;
@@ -20,5 +19,33 @@ public class Juice {
 
 	public boolean isEnough(int amount) {
 		return amount >= this.getPrice();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((juiceName == null) ? 0 : juiceName.hashCode());
+		result = prime * result + price;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Juice other = (Juice) obj;
+		if (juiceName == null) {
+			if (other.juiceName != null)
+				return false;
+		} else if (!juiceName.equals(other.juiceName))
+			return false;
+		if (price != other.price)
+			return false;
+		return true;
 	}
 }
