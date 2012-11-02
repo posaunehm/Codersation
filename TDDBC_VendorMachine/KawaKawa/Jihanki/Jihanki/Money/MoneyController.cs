9157137045d9746607fcb89083fbe8f771201ed1
Takuya Kawabe <rapidarrow2000.github@gmail.com>
2012-11-03 03:49:41 +0900

    Step0：お金の投入という挙動を管理するコントローラーを追加

---
+++ TDDBC_VendorMachine/KawaKawa/Jihanki/Jihanki/Money/MoneyController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jihanki.Money
+{
+    /// <summary>
+    /// お金の管理を行います。
+    /// </summary>
+    public class MoneyController
+    {
+
+        /// <summary>
+        /// 投入金額
+        /// </summary>
+        private Base.Moneys inputMoney = new InputMoney();
+
+
+        /// <summary>
+        /// ユーザからの投入金を追加
+        /// </summary>
+        /// <param name="money">投入金額</param>
+        public void InputMoneyAdd(Base.Money money)
+        {
+            inputMoney.Add(money);
+        }
+
+
+
+
+
+
+    }
+}
---
+++ TDDBC_VendorMachine/KawaKawa/Jihanki/Jihanki/OperateController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jihanki
+{
+
+    public class OperateController
+    {
+        /// <summary>
+        /// お金の受付関係
+        /// </summary>
+        private ReceiptMoney receiptMoney = new ReceiptMoney();
+
+
+
+
+        /// <summary>
+        /// ユーザからの投入金の受付
+        /// </summary>
+        /// <param name="money">投入金</param>
+        public void ReceiptMoney(Money.Base.Money money)
+        {
+            this.receiptMoney.Receipt(money);
+        }
+
+
+
+    }
+}
