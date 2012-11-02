ff06e0d04f404394c7d63e916a24d8285e0d280c
Takuya Kawabe <rapidarrow2000.github@gmail.com>
2012-11-02 21:04:21 +0900

    Step0：投入銀額やつり銭をあらわすスーパークラスを作成

---
+++ TDDBC_VendorMachine/KawaKawa/Jihanki/Jihanki/Money/Base/Moneys.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jihanki.Money.Base
+{
+    public class Moneys
+    {
+        private List<Money> stock= new List<Money>();
+
+
+        public void Add(Money money)
+        {
+            this.stock.Add(money);
+        }
+
+
+
+
+    }
+}
