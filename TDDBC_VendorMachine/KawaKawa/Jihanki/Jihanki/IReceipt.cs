66ff1a90205109630ae0a03b303ac58f44267e5b
Takuya Kawabe <rapidarrow2000.github@gmail.com>
2012-11-02 13:41:49 +0900

    Step0：お金投入の受付処理＆テスト追加

--- TDDBC_VendorMachine/KawaKawa/Jihanki/Jihanki/IReceipt.cs
+++ TDDBC_VendorMachine/KawaKawa/Jihanki/Jihanki/IReceipt.cs
@@ -12,8 +12,12 @@ namespace Jihanki
         /// <summary>
         /// お金を受け付ける
         /// </summary>
-        /// <param name="money"></param>
-        void Receipt(Money.Base.Money money);
+        /// <param name="money">投入されたお金</param>
+        /// <returns>受付結果
+        /// true･･･成功
+        /// false･･･失敗
+        /// </returns>
+        bool Receipt(Money.Base.Money money);
 
 
     }
---
+++ TDDBC_VendorMachine/KawaKawa/Jihanki/Jihanki/ReceiptMoney.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jihanki
+{
+    /// <summary>
+    /// 挿入されたお金の受け入れ
+    /// </summary>
+    public class ReceiptMoney:IReceipt
+    {
+
+        /// <summary>
+        /// ユーザなどが挿入したお金の受け入れ
+        /// </summary>
+        /// <param name="money"></param>
+        public bool Receipt(Money.Base.Money money)
+        {
+            return true;
+        }
+    }
+}
