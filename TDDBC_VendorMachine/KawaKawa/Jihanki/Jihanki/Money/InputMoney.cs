93639bd75e43bacb6a1382e481b3eae1f4528d9e
Takuya Kawabe <rapidarrow2000.github@gmail.com>
2012-11-03 03:51:29 +0900

    Step0：投入金のデータ保持用Class

---
+++ TDDBC_VendorMachine/KawaKawa/Jihanki/Jihanki/Money/InputMoney.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jihanki.Money
+{
+    /// <summary>
+    /// 投入金額
+    /// </summary>
+    public class InputMoney:Base.Moneys
+    {
+    }
+}
