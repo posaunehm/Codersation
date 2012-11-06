fbecf8bd686c5ea2e374d52a782b0b0686294049
Takuya Kawabe <rapidarrow2000.github@gmail.com>
2012-11-06 22:04:39 +0900

    Step2：Drinkの基本Class追加

---
+++ TDDBC_VendorMachine/KawaKawa/Jihanki/Jihanki/DrinkrRlations/Base/Drink.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jihanki.DrinkrRlations.Base
+{
+    public class Drink
+    {
+        public int Price;
+        public string Name;
+
+
+
+
+
+    }
+}
