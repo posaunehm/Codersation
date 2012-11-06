49789b9c6e49c8f911b04de45689ccb5219680b6
Takuya Kawabe <rapidarrow2000.github@gmail.com>
2012-11-06 12:51:08 +0900

    Step1：取り扱い可能な金種別か調べる（レッド）

---
+++ TDDBC_VendorMachine/KawaKawa/Jihanki/Jihanki/Cashier/HandingMoney.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jihanki.Cashier
+{
+    /// <summary>
+    /// 自販機が取り扱う金額種別を判定
+    /// </summary>
+    public class HandingMoney
+    {
+
+        /// <summary>
+        /// 取り扱いしているお金かチェック
+        /// </summary>
+        /// <param name="money"></param>
+        /// <returns></returns>
+        public bool IsHandling(Base.Money money)
+        {
+            return false;
+        }
+
+
+
+    }
+}
---
+++ TDDBC_VendorMachine/KawaKawa/Jihanki/Jihanki/TEST/Cashier/HandingMoneyTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Jihanki.TEST.Cashier
+{
+         
+    [TestFixture]
+    class HandingMoneyTest
+    {
+
+        [Test]
+        public void 取り扱い可能なお金種別であることを確認()
+        {
+            var target = new Jihanki.Cashier.HandingMoney();
+
+            //投入するお金
+            var yen = new Jihanki.Cashier.Base.Money(Jihanki.Cashier.Base.MoneyKind.Kind.Yen1);
+
+
+            var expect = true;
+            var actual = target.IsHandling(yen);
+
+            Assert.AreEqual(expect, actual);
+
+        }
+
+
+    }
+}
