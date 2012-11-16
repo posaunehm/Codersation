bec49ac92b6b9dde67773d2912c22eaebfcd0579
Ritalin <kazu.ktz.tamura@gmail.com>
2012-11-17 02:35:29 +0900

    最も単純なワード補完を実装した

--- TDDBC_VendorMachine/ktz_alias/sources/VendingMachine.Console.Test/ConsoleBufferTest.cs
+++ TDDBC_VendorMachine/ktz_alias/sources/VendingMachine.Console.Test/ConsoleBufferTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 using NUnit.Framework;
@@ -6,21 +7,21 @@ using NUnit.Framework;
 namespace VendingMachine.Console.Test {
     [TestFixture]
     public class _Consoleのバッファ制御に関するTestSuite {
-        [TestCase("ins\n")] 
-        [TestCase("buy\n")]
-        [TestCase("show ins\n")]
-        [TestCase("show item\n")]
-        [TestCase("show amount\n")]
-        [TestCase("eject\n")] 
-        [TestCase("help\n")]
-        [TestCase("help ins\n")] 
-        [TestCase("help buy\n")]
-        [TestCase("help show ins\n")]
-        [TestCase("help show item\n")]
-        [TestCase("help show amount\n")]
-        [TestCase("help elect\n")]
-        public void TestCase(string inExpected) {
-            var buf = new FakeConsoleReadBuffer(inExpected) {
+        [TestCase("ins")] 
+        [TestCase("buy")]
+        [TestCase("show ins")]
+        [TestCase("show item")]
+        [TestCase("show amount")]
+        [TestCase("eject")] 
+        [TestCase("help")]
+        [TestCase("help ins")] 
+        [TestCase("help buy")]
+        [TestCase("help show ins")]
+        [TestCase("help show item")]
+        [TestCase("help show amount")]
+        [TestCase("help elect")]
+        public void _標準入力からの取得をシミュレーション(string inExpected) {
+            var buf = new FakeConsoleReadBuffer(new string[0], inExpected) {
                 Prompt = "> ",
             };
 
@@ -29,28 +30,97 @@ namespace VendingMachine.Console.Test {
             Assert.That(buf.Current, Is.Null.Or.Empty);
 
             foreach (var e in inExpected.Select((c, i) => Tuple.Create(i, c))) {
-                Assert.That(buf.Cursor, Is.EqualTo(e.Item1));
+                Assert.That(buf.Read(), Is.EqualTo(e.Item2));
+
+                Assert.That(buf.Cursor, Is.EqualTo(e.Item1+1));
                 Assert.That(buf.Current, Is.EqualTo(inExpected.Substring(0, buf.Cursor)));
                 Assert.That(buf.History.Any(), Is.False);
-
-                Assert.That(buf.Read(), Is.EqualTo(e.Item2));
             }
+
+            Assert.That(buf.ReadExtraNewLine(), Is.EqualTo('\n'));
+
             Assert.That(buf.Cursor, Is.EqualTo(0));
             Assert.That(buf.Current, Is.Null.Or.Empty);
             Assert.That(buf.History.Any(), Is.True);
             Assert.That(buf.History.Last(), Is.EqualTo(inExpected.Trim()));
+        }
+
+        [TestCase("in", "ins")] 
+        [TestCase("bu", "buy")]
+        [TestCase("eje", "eject")] 
+        public void _標準入力からの取得をシミュレーション_入力補完あり(string inPartial, string inExpected) {
+            var dic = new List<string> {
+                "ins",
+                "buy",
+                "show ins",
+                "show item",
+                "show amount",
+                "eject", 
+                "help",
+                "help ins", 
+                "help buy",
+                "help show ins",
+                "help show item",
+                "help show amount",
+                "help elect",
+            };
+
+            var buf = new FakeConsoleReadBuffer(dic, inPartial) {
+                Prompt = "> ",
+            };
+
+            Assert.That(buf.InCompleting, Is.False);
+
+            buf.ReadToEnd();
+            Assert.That(buf.Current, Is.EqualTo(inPartial));
+            Assert.That(buf.InCompleting, Is.False);
+
+            Assert.That(buf.ReadExtraTab(), Is.EqualTo(inExpected.Last()));
+            Assert.That(buf.Current, Is.EqualTo(inExpected));
+            Assert.That(buf.InCompleting, Is.True);
+
+            Assert.That(buf.ReadExtraTab(), Is.EqualTo(inExpected.Last()));
+            Assert.That(buf.Current, Is.EqualTo(inExpected));
+            Assert.That(buf.InCompleting, Is.True);
+
+            Assert.That(buf.ReadExtraSpace(), Is.EqualTo(' '));
+            Assert.That(buf.Current, Is.EqualTo(inExpected + ' '));
+            Assert.That(buf.InCompleting, Is.False);
+
         }
     }
 
     internal class FakeConsoleReadBuffer : AbstractConsoleReadBuffer {
         private string mExpected;
 
-        public FakeConsoleReadBuffer(string inExpected) {
+        public FakeConsoleReadBuffer(IEnumerable<string> inDictionary, string inExpected) : base(inDictionary) {
             mExpected = inExpected;
         }
 
         protected override char ReadCore() {
-            return mExpected[this.Cursor++];
+            return this.Cursor == mExpected.Length ? '\0' : mExpected[this.Cursor++];
+        }
+
+        public void ReadToEnd() {
+            while (this.Read() != '\0');
+        }
+
+        private char ReadExtraChar(char inChar) {
+            mExpected += inChar;
+
+            return this.Read();
+        }
+
+        public char ReadExtraNewLine() {
+            return this.ReadExtraChar('\n');
+        }
+
+        public char ReadExtraTab() {
+            return this.ReadExtraChar('\t');
+        }
+
+        public char ReadExtraSpace() {
+            return this.ReadExtraChar(' ');
         }
     }
 }
--- TDDBC_VendorMachine/ktz_alias/sources/VendingMachine.Console/ConsoleBuffer.cs
+++ TDDBC_VendorMachine/ktz_alias/sources/VendingMachine.Console/ConsoleBuffer.cs
@@ -1,15 +1,29 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace VendingMachine.Console {
     public abstract class AbstractConsoleReadBuffer {
         private StringBuilder mBuf;
+        private IEnumerable<string> mDic;
+        private string mCompletement;
+        private Func<string> mGenerator;
 
-        protected AbstractConsoleReadBuffer() {
+        protected AbstractConsoleReadBuffer(IEnumerable<string> inPhrases) {
+            mDic = this.MakeDictionary(inPhrases);
+
             this.ClearBuffer();
 
             this.History = new List<string>();
+        }
+
+        private IEnumerable<string> MakeDictionary(IEnumerable<string> inPhrases) {
+            return inPhrases
+                .Select(p => p.Split(' '))
+                .SelectMany(words => words.Select((_, i) => string.Join(" ", words, 0, i+1)))
+                .ToList()
+            ;
         }
 
         private void ClearBuffer() {
@@ -24,8 +38,23 @@ namespace VendingMachine.Console {
                 this.History.Add(this.Current);
 
                 this.ClearBuffer();
+            } 
+            else if (c == '\t') {
+                if (! this.InCompleting) {
+                    mGenerator = this.InitCompletement(this.GetCurrentText(false));
+                }
+
+                mCompletement = mGenerator();
+
+                c = string.IsNullOrEmpty(mCompletement) ? '\0': mCompletement.Last();
             }
-            else {
+            else if (c != '\0') {
+                if (this.InCompleting) {
+                    mBuf = new StringBuilder(mCompletement);
+                    mGenerator = null;
+                    mCompletement = null;
+                }
+
                 mBuf.Append(c);
             }
 
@@ -34,11 +63,49 @@ namespace VendingMachine.Console {
 
         protected abstract char ReadCore();
 
+        private Func<string> InitCompletement(string inTarget) {
+            var it = InitCompletementCore(inTarget);
+
+            return () => {
+                it.MoveNext();
+
+                return it.Current;
+            };  
+        }
+
+        private IEnumerator<string> InitCompletementCore(string inTarget) {
+            var items = mDic
+                .Where(phrase => string.IsNullOrWhiteSpace(inTarget) || phrase.StartsWith(inTarget))
+            ;
+
+            var noMatch = ! items.Any();
+            while (true) {
+                if (noMatch) {
+                    yield return inTarget;
+                }
+                else {
+                    foreach (var item in items) {
+                        yield return item;
+                    }
+                }
+            }
+        }
+
+        public bool InCompleting { 
+            get {
+                return mGenerator != null;
+            } 
+        }
+
         public string Prompt { get; set; }
         public string Current { 
             get {
-                return mBuf.ToString();
+                return this.GetCurrentText(this.InCompleting);
             } 
+        }
+
+        private string GetCurrentText(bool inInCompleting) {
+            return inInCompleting ? mCompletement : mBuf.ToString();
         }
 
         public int Cursor { get; set; }
