f41cc022df98a833e2ce23b71e77eb3bf51b888e
Aki <aki.onetime@gmail.com>
2012-11-29 21:21:04 +0900

    fix step0.

--- TDDBC_VendorMachine/springaki/VendingMachine/src/com/github/springaki/codersation/VendingMachine.java
+++ TDDBC_VendorMachine/springaki/VendingMachine/src/com/github/springaki/codersation/VendingMachine.java
@@ -1,8 +1,23 @@
 package com.github.springaki.codersation;
 
+
 public class VendingMachine {
 
-	public void InsertMoney(Money money) {
-		
+//	private List<Money> amount = new ArrayList<Money>();
+	private int totalAmount = 0;
+	
+	public void insertMoney(Money money) {
+//		amount.add(money);
+		this.totalAmount += money.toInt();
+	}
+
+	public int getTotalAmount() {
+		return this.totalAmount;
+	}
+
+	public int calcel() {
+		int cancelAmount = this.totalAmount;
+		this.totalAmount = 0;
+		return cancelAmount;
 	}
 }
--- TDDBC_VendorMachine/springaki/VendingMachine/src/test/com/github/springaki/codersation/VendingMachineTest.java
+++ TDDBC_VendorMachine/springaki/VendingMachine/src/test/com/github/springaki/codersation/VendingMachineTest.java
@@ -1,5 +1,7 @@
 package test.com.github.springaki.codersation;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import com.github.springaki.codersation.Money;
@@ -11,12 +13,39 @@ public class VendingMachineTest {
 
 	@Test
 	public void testInsertMoney() {
-		sut.InsertMoney(Money.Ten);
+		sut.insertMoney(Money.Ten);
 	}
 
 	@Test
 	public void _10‰~‹Ê_50‰~‹Ê_100‰~‹Ê_500‰~‹Ê_1000‰~D‚ğ‚P‚Â‚¸‚Â“Š“ü‚Å‚«‚é() {
-		sut.InsertMoney(Money.Ten);		
+		sut.insertMoney(Money.Ten);
+		sut.insertMoney(Money.Fifty);		
+		sut.insertMoney(Money.OneHundred);		
+		sut.insertMoney(Money.FiveHundred);		
+		sut.insertMoney(Money.OneThousand);		
+	}
+
+	@Test
+	public void “Š“ü‚Í•¡”‰ñ‚Å‚«‚é() {
+		sut.insertMoney(Money.Ten);
+		sut.insertMoney(Money.Ten);
+	}
+
+	@Test
+	public void “Š“ü‹àŠz‚Ì‘Œv‚ğæ“¾‚Å‚«‚é() {
+		assertEquals(0, sut.getTotalAmount());		
+		sut.insertMoney(Money.Ten);
+		sut.insertMoney(Money.Ten);
+		assertEquals(20, sut.getTotalAmount());		
+	}
+	
+	@Test
+	public void •¥‚¢–ß‚µ‘€ì‚ğs‚¤‚Æ“Š“ü‹àŠz‚Ì‘Œv‚ğ’Ş‚è‘K‚Æ‚µ‚Äo—Í‚·‚é() {
+		assertEquals(0, sut.calcel());
+		sut.insertMoney(Money.Ten);
+		sut.insertMoney(Money.Ten);
+		assertEquals(20, sut.calcel());
+		assertEquals(0, sut.calcel());
 	}
 
 }
