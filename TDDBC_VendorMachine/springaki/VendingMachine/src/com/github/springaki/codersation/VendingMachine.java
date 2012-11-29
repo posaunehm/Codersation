3f4aa63f9bf2c8f9823a90e4eb6f67f9d34fa36f
Aki <aki.onetime@gmail.com>
2012-11-29 21:22:01 +0900

    delete comment logic.

--- TDDBC_VendorMachine/springaki/VendingMachine/src/com/github/springaki/codersation/VendingMachine.java
+++ TDDBC_VendorMachine/springaki/VendingMachine/src/com/github/springaki/codersation/VendingMachine.java
@@ -3,11 +3,9 @@ package com.github.springaki.codersation;
 
 public class VendingMachine {
 
-//	private List<Money> amount = new ArrayList<Money>();
 	private int totalAmount = 0;
 	
 	public void insertMoney(Money money) {
-//		amount.add(money);
 		this.totalAmount += money.toInt();
 	}
 
