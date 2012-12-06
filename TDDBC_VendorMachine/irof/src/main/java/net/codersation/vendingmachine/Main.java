e208f488535ca8d21a9f3d709c24ea10bfe9957e
irof <irof@hogedriven.net>
2012-12-06 21:37:49 +0900

    あるところにMainクラスがございまして

---
+++ TDDBC_VendorMachine/irof/src/main/java/net/codersation/vendingmachine/Main.java
@@ -0,0 +1,8 @@
+package net.codersation.vendingmachine;
+
+public class Main {
+
+	public static void main(String... args) {
+		
+	}
+}
---
+++ TDDBC_VendorMachine/irof/src/test/java/net/codersation/vendingmachine/MainTest.java
@@ -0,0 +1,14 @@
+package net.codersation.vendingmachine;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void testName() throws Exception {
+		Main.main();
+	}
+}
