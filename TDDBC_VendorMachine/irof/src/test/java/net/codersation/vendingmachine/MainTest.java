5e765d80b74cd4bbac35bb0b4a9adf1c18a59f97
irof <irof@hogedriven.net>
2012-12-06 23:33:13 +0900

    コンソールのテストコードのリファクタリングを終えた感

--- TDDBC_VendorMachine/irof/src/test/java/net/codersation/vendingmachine/MainTest.java
+++ TDDBC_VendorMachine/irof/src/test/java/net/codersation/vendingmachine/MainTest.java
@@ -17,33 +17,43 @@ import org.junit.rules.TestWatcher;
 public class MainTest {
 
 	@Rule
-	public SysOutReader reader = new SysOutReader();
+	public SystemOut out = new SystemOut();
+	public SystemIn in = new SystemIn();
 
 	@Test
 	public void お金の投入はinsでする() throws Exception {
-		byte[] input = "ins 100\nins 10\n".getBytes();
-		System.setIn(new ByteArrayInputStream(input));
+		in.writeLine("ins 100");
+		in.writeLine("ins 10");
 
 		Main.main();
 
-		assertThat(reader.readLine(), is("> "));
-		assertThat(reader.readLine(), is("money: 100 was received."));
-		assertThat(reader.readLine(), is("> "));
-		assertThat(reader.readLine(), is("money: 10 was received."));
+		assertThat(out.readLine(), is("> "));
+		assertThat(out.readLine(), is("money: 100 was received."));
+		assertThat(out.readLine(), is("> "));
+		assertThat(out.readLine(), is("money: 10 was received."));
 	}
 
 	@Test
 	public void 変なお金をinsしたらば() throws Exception {
-		byte[] input = "ins 123".getBytes();
-		System.setIn(new ByteArrayInputStream(input));
+		in.writeLine("ins 123");
 
 		Main.main();
 
-		assertThat(reader.readLine(), is("> "));
-		assertThat(reader.readLine(), is("! 123 is not available."));
+		assertThat(out.readLine(), is("> "));
+		assertThat(out.readLine(), is("! 123 is not available."));
 	}
 
-	static class SysOutReader extends TestWatcher {
+	static class SystemIn {
+		StringBuilder sb = new StringBuilder();
+
+		public void writeLine(String str) {
+			sb.append(str);
+			sb.append(System.lineSeparator());
+			System.setIn(new ByteArrayInputStream(sb.toString().getBytes()));
+		}
+	}
+
+	static class SystemOut extends TestWatcher {
 		ByteArrayOutputStream baos;
 		BufferedReader reader;
 
