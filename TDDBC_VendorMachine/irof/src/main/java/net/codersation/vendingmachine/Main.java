1fbd634b37519a6927812b53a2b87a0ccacbeaf2
irof <irof@hogedriven.net>
2012-12-06 21:57:35 +0900

    どうやら入力をそのまま返すようだ

--- TDDBC_VendorMachine/irof/src/main/java/net/codersation/vendingmachine/Main.java
+++ TDDBC_VendorMachine/irof/src/main/java/net/codersation/vendingmachine/Main.java
@@ -1,8 +1,12 @@
 package net.codersation.vendingmachine;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String... args) {
-		
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.print(scanner.nextLine());
+		}
 	}
 }
--- TDDBC_VendorMachine/irof/src/test/java/net/codersation/vendingmachine/MainTest.java
+++ TDDBC_VendorMachine/irof/src/test/java/net/codersation/vendingmachine/MainTest.java
@@ -3,12 +3,26 @@ package net.codersation.vendingmachine;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
 import org.junit.Test;
 
 public class MainTest {
 
 	@Test
 	public void testName() throws Exception {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(baos));
+
+		byte[] input = "hoge".getBytes();
+		System.setIn(new ByteArrayInputStream(input));
+
 		Main.main();
+
+		String actual = baos.toString("UTF-8");
+		assertThat(actual, is("hoge"));
 	}
 }
