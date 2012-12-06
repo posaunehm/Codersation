385e8187c02a34f5d79392655747bf074b6b7dec
irof <irof@hogedriven.net>
2012-12-06 23:50:21 +0900

    メッセージ取得用のメソッド追加

--- TDDBC_VendorMachine/irof/src/test/java/net/codersation/vendingmachine/MainTest.java
+++ TDDBC_VendorMachine/irof/src/test/java/net/codersation/vendingmachine/MainTest.java
@@ -18,29 +18,32 @@ public class MainTest {
 
 	@Rule
 	public SystemOut out = new SystemOut();
-	public SystemIn in = new SystemIn();
+	public SystemIn in = SystemIn.create();
 
 	@Test
-	public void お金の投入はinsでする() throws Exception {
+	public void 入力待ちを表示する() throws Exception {
+		Main.main();
+		assertThat(out.readLine(), is("> "));
+	}
+
+	@Test
+	public void insでお金を入れるとメッセージが出る() throws Exception {
 		in.writeLine("ins 100");
 		in.writeLine("ins 10");
 
 		Main.main();
 
-		assertThat(out.readLine(), is("> "));
-		assertThat(out.readLine(), is("money: 100 was received."));
-		assertThat(out.readLine(), is("> "));
-		assertThat(out.readLine(), is("money: 10 was received."));
+		assertThat(out.readMesage(), is("money: 100 was received."));
+		assertThat(out.readMesage(), is("money: 10 was received."));
 	}
 
 	@Test
-	public void 変なお金をinsしたらば() throws Exception {
+	public void 変な金額をinsしたらエラーメッセージ() throws Exception {
 		in.writeLine("ins 123");
 
 		Main.main();
 
-		assertThat(out.readLine(), is("> "));
-		assertThat(out.readLine(), is("! 123 is not available."));
+		assertThat(out.readMesage(), is("! 123 is not available."));
 	}
 
 	static class SystemIn {
@@ -50,6 +53,12 @@ public class MainTest {
 			sb.append(str);
 			sb.append(System.lineSeparator());
 			System.setIn(new ByteArrayInputStream(sb.toString().getBytes()));
+		}
+
+		public static SystemIn create() {
+			SystemIn instance = new SystemIn();
+			System.setIn(new ByteArrayInputStream(new byte[0]));
+			return instance;
 		}
 	}
 
@@ -61,6 +70,11 @@ public class MainTest {
 			baos = new ByteArrayOutputStream();
 			System.setOut(new PrintStream(baos));
 		};
+
+		public String readMesage() throws IOException {
+			String line = readLine();
+			return line.equals("> ") ? readMesage() : line;
+		}
 
 		public String readLine() throws IOException {
 			if (reader == null) {
